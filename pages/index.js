8caf8f13386584eee55f53beeff03b7a6cdd0ae2
arammath <tamlerner@gmail.com>
2025-02-02 12:40:42 +0100

    Initial commit: RootstockCollective Operations Dashboard

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import OperationsDashboard from '../components/dashboard/OperationsDashboard'
+
+export default function Home() {
+  return (
+    <div>
+      <OperationsDashboard />
+    </div>
+  )
+}
